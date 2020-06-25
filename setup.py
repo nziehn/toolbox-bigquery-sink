0c1ed9ea871f01ce50aa6be33615a6c74532bb10
Nils Ziehn <nziehn@googlemail.com>
2020-06-26 00:22:04 +0200

    Version 0.0.21: improved ensure of DATE fields

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ dependencies = [
 
 setuptools.setup(
     name='toolbox-bigquery-sink',
-    version='0.0.20',
+    version='0.0.21',
     description='Tooling to write data to google bigquery',
     # long_description=long_description,
     # long_description_content_type='text/markdown',
