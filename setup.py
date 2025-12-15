41f29a2fcbf402112bfccd619c65d434aca5235e
Nils Ziehn <nziehn@googlemail.com>
2025-12-15 10:18:54 +0100

    Version 0.0.33: Python 3.13 compatible

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ dependencies = [
 
 setuptools.setup(
     name='toolbox-bigquery-sink',
-    version='0.0.32',
+    version='0.0.33',
     description='Tooling to write data to google bigquery',
     # long_description=long_description,
     # long_description_content_type='text/markdown',
